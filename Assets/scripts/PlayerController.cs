715c06576a1ac866abe0a26de9424b65b964c4b5
Bruno Silvagit config --global user.name Bruno <1111782@isep.ipp.pt>
2015-08-06 08:57:46 +0100

    init

---
+++ Assets/scripts/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed;
+
+	private Rigidbody rb;
+
+	void Start()
+	{
+		rb = GetComponent<Rigidbody>();
+	}
+
+	void FixedUpdate()
+	{
+		float moveH = Input.GetAxis("Horizontal");
+		float moveV = Input.GetAxis("Vertical");
+
+		Vector3 movement = new Vector3(moveH,0.0f,moveV);
+
+		rb.AddForce(movement * speed);
+	 
+	}
+	void OnTriggerEnter(Collider other) 
+	{
+		if (other.gameObject.CompareTag ("Pick Up"))
+		{
+			other.gameObject.SetActive (false);
+		}
+	}
+}
